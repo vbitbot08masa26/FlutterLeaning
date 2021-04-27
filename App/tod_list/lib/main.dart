1cb9da0600f936ea41a310c186a84ba0d6437609
vbitbot08masa26 <hiroike0826masaya2020@gmail.com>
2021-04-27 09:09:39 +0900

    ver.2021.04.27.09.09

--- App/tod_list/lib/main.dart
+++ App/tod_list/lib/main.dart
@@ -19,10 +19,65 @@ class MyApp extends StatelessWidget {
 
 class TodoListPage extends StatelessWidget {
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト一覧画面'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('ニンジンを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('タマネギを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('ジャガイモを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('カレールーを買う'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       body: Center(
-        child: Text('リスト一覧画面'),
+        child: TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面(クリックで戻る)'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
       ),
     );
   }
